ceeec2a83da4283a25d67591ebea80be7e7a224b
Ayan <ayanhasnain2572006@gmail.com>
2025-06-26 21:43:48 +0530

    12:agents-data-table

---
+++ src/components/empty-state.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+interface Props {
+  title: string;
+  description: string;
+  iconUrl?: string;
+}
+
+export const EmptyState = ({
+  title,
+  description,
+  iconUrl = "/empty.svg",
+}: Props) => {
+  return (
+    <div
+      className="flex items-center justify-center text-center flex-col mx-auto
+"
+    >
+      <Image
+        src={iconUrl}
+        alt="Empty State"
+        height={200}
+        width={400}
+        className="h-[250px]"
+        priority
+      />
+      <div className="">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          {title}
+        </h1>
+        <p className="text-base text-muted-foreground max-w-md mx-auto leading-relaxed">
+          {description}
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -4,17 +4,25 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "@/modules/agents/ui/components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
   const trpc = useTRPC();
   // get  data from cache
-  const { data } = useSuspenseQuery({
-    ...trpc.agents.getMany.queryOptions(),
-  });
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
   return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns}/>
+      {
+        data.length === 0 && (
+          <EmptyState
+          title="Create Your first agent"
+          description="Create an agent to join your meetings. Each agents will follow your instructions and can interact with participants during the call. "/>
+        )
+      }
     </div>
   );
 };
