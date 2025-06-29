18352ccf2501942f4ab0557a81f101b3234dff3e
Ayan <ayanhasnain2572006@gmail.com>
2025-06-29 08:04:13 +0530

    16: Meeting setup

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,8 +2,10 @@
 import { z } from "zod";
 import { createTRPCRouter } from "../init";
 import { agentsRouter } from "@/modules/agents/server/procedures";
+import { meetingsRouter } from "@/modules/meetings/server/procedures";
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
+  meetings:meetingsRouter
 
 });
 // export type definition of API
